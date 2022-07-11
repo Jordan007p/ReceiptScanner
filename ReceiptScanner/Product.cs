c0bd9bd14e425d790a8c4e1330eb7bf490b3e702
Jordan <Jordan@JORDANPC>
2022-07-11 02:26:24 +0200

    Add project files.

---
+++ ReceiptScanner/Product.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiptScanner
+{
+
+    public class Product
+    {
+        // Deserialized json constructed class with non nullable variables. Meaning all behaviour will be predictable.
+        public string name { get; set; }
+        public int domestic { get; set; }
+        public float price { get; set; }
+        public int weight { get; set; }
+        public string description { get; set; }
+
+        // Nullsafe constructor for product so any variable can be null and not effect final tally.
+        public Product(string? name,
+                       bool? domestic,
+                       float? price,
+                       int? weight,
+                       string? description)
+        {
+            if (name == null)
+            {
+                this.name = string.Empty;
+            }else
+            {
+                this.name = name;
+            }
+            if (domestic == null)
+            {
+                this.domestic = 3;
+            }
+            else if (domestic == true)
+            {
+                this.domestic = 1;
+            }
+            else
+            {
+                this.domestic = 0;
+            }
+            if (price == null)
+            {
+                this.price = 0;
+            }else
+            {
+                this.price = (float)price;
+            }
+            if (weight == null)
+            {
+                this.weight = 0;
+            }else
+            {
+                this.weight = (int)weight;
+            }
+            if (description == null)
+            {
+                this.description = string.Empty;
+            }else
+            {
+                this.description = description;
+            }
+        }
+
+        // Simple printAll() func for product with checks for zeroes.
+        public void printAll()
+        {
+            if (name.Length > 0)
+            {
+                Console.WriteLine(name);
+            }
+            if (price != 0)
+            {
+                Console.WriteLine("    " + "Price: $" + price.ToString("N1"));
+            }
+            if (description.Length >= 10)
+            {
+                Console.WriteLine("    " + description.Substring(0, 10) + "...");
+            }
+            else if (description.Length != 0)
+            {
+                Console.WriteLine("    " + description);
+            }
+            if (weight != 0)
+            {
+                Console.WriteLine("    " + "Weight: " + weight + "g");
+            }
+            else
+            {
+                Console.WriteLine("    " + "Weight: N/A");
+            }
+        }
+    }
+
+}
---
+++ ReceiptScanner/Program.cs
@@ -0,0 +1,49 @@
+﻿using ReceiptScanner;
+
+//Simple text entry for file path. Example C:\Users\Jordan\source\repos\ReceiptScanner\ReceiptScanner\alpha-qr-gFpwhsQ8fkY1.json
+Console.WriteLine("Please enter location of related json file as full path verbatim:");
+string location = Console.ReadLine();
+string jsonText = File.ReadAllText(@location);
+
+//Deserializer for json file meaning all product are direct json translations.
+List<Product> list = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Product>>(jsonText);
+list = list.OrderBy(x => x.name).ToList();
+
+
+//Set of counters for price and number of products.
+int domesticCounter = 0;
+int importedCounter = 0;
+float domesticTotal = 0;
+float importedTotal = 0;
+
+//Domestic printer.
+Console.WriteLine(".Domestic");
+foreach (Product product in list)
+{
+    if (product.domestic==1)
+    {
+        Console.Write("... ");
+        product.printAll();
+        domesticCounter++;
+        domesticTotal += product.price;
+    }
+}
+
+//Imported printer.
+Console.WriteLine(".Imported");
+foreach (Product product in list)
+{
+    if (product.domestic==0)
+    {
+        Console.Write("... ");
+        product.printAll();
+        importedCounter++;
+        importedTotal+=product.price;
+    }
+}
+
+//Final tally of all counters and totals.
+Console.WriteLine("Domestic cost: $"+domesticTotal.ToString("N1"));
+Console.WriteLine("Imported cost: $"+importedTotal.ToString("N1"));
+Console.WriteLine("Domestic count: "+domesticCounter);
+Console.WriteLine("Imported count: "+importedCounter);
